954dce93d68c64a97e403a767adfffa3b2e31fbe
mccomasd <mccomasd@gmail.com>
2015-11-10 06:41:45 -0500

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_HOST','localhost');
+define('DB_USER','cl13-phplovers');
+define('DB_PASS','yfYjfH7.3');
+define('DB_NAME','cl13-phplovers');
+
+$site_description = 'Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.';
